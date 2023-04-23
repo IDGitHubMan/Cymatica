0bd77888ed41af7f0ef229a2368910d232597023
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-04-23 19:54:44 -0400

    Create method for adding songs

--- CymaticaV3/Controller.py
+++ CymaticaV3/Controller.py
@@ -1,10 +1,12 @@
 from PyQt6.QtWidgets import QApplication, QWidget
 import sys
 import json
+import eyed3
 import os
 import csv
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtCore import QSize, pyqtSlot
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+import shutil
 
 # Subclass QMainWindow to customize your application's main window
 
@@ -17,9 +19,9 @@ class MainWindow(QMainWindow):
             library = open(os.path.join(here, 'data/library.csv'), "r")
         except OSError as error:
             with open(os.path.join(here, 'data/library.csv'), "a") as library:
-                fieldnames = ["Title", "Path", "Album", "Artist", "Genre", "ArtPath", "LeftR", "LeftG", "LeftB", "LeftA", "RightR", "RightG", "RightB", "RightA", "MixR", "MixG",
+                fieldnames = ["Title", "OrigPath", "LocalPath", "Album", "Artist", "Genre", "ArtPath", "LeftR", "LeftG", "LeftB", "LeftA", "RightR", "RightG", "RightB", "RightA", "MixR", "MixG",
                               "MixB", "MixA", "Laser", "Spark", "Line", "LaserMin", "LaserMax", "LaserThreshold", "SparkMin", "SparkMax", "SparkThreshold", "LineMin", "LineMax", "LineThreshold"]
-                writer = csv.DictWriter(queue, fieldnames=fieldnames)
+                writer = csv.DictWriter(library, fieldnames=fieldnames)
                 writer.writeheader()
         try:
             states = open(os.path.join(here, 'data/states.csv'), "r")
@@ -36,12 +38,37 @@ class MainWindow(QMainWindow):
             print("Folder is already made!")
         super().__init__()
         self.setWindowTitle("Cymatica")
-        button = QPushButton("Press Me!")
+        button = QPushButton("Add Song")
 
         self.setMinimumSize(QSize(600, 600))
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
+        button.clicked.connect(self.local_add)
+
+    @pyqtSlot()
+    def local_add(self):
+        here = os.path.dirname(os.path.abspath(__file__))
+        fname = QFileDialog.getOpenFileName(
+            self,
+            "Open File",
+            "${HOME}",
+            "Audio Files (*.mp3 *.wav);; All Files (*)",
+        )
+        a = eyed3.load(fname[0])
+        shutil.copy(fname[0], os.path.join(
+            here, 'data/Library/Music'))
+        print()
+        if a.tag.title != "":
+            with open(os.path.join(here, 'data/library.csv'), "a") as library:
+                writer = csv.writer(library, delimiter=',', quotechar='"')
+                writer.writerow([a.tag.title, fname[0], os.path.join(
+                    here, 'data/Library/Music' + fname[0][fname[0].rindex("/"): len(fname[0])]), a.tag.album, a.tag.artist, a.tag.genre, "", 0, 255, 255, 255, 255, 0, 0, 255, 255, 255, 255, 255, False, False, False, 0, 20, 40, 50, 100, 100, 50, 100, 100])
+        else:
+            with open(os.path.join(here, 'data/library.csv'), "a") as library:
+                writer = csv.writer(library, delimiter=',', quotechar='"')
+                writer.writerow([fname[0][fname[0].rindex("/"): -4], fname[0], os.path.join(
+                    here, 'data/Library/Music' + fname[0][fname[0].rindex("/"): len(fname[0])]), a.tag.album, a.tag.artist, a.tag.genre, "", 0, 255, 255, 255, 255, 0, 0, 255, 255, 255, 255, 255, False, False, False, 0, 20, 40, 50, 100, 100, 50, 100, 100])
 
 
 app = QApplication(sys.argv)
